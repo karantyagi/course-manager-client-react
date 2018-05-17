483d59ca8069d91fc28c40026964408f5cebc35e
karantyagi <tyagi.k@husky.neu.edu>
2018-05-17 17:40:21 -0400

    added first component

---
+++ src/hello.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class HelloWorld
+    extends React.Component {
+    render() {      // render tells us what we want to display as component
+        return(
+        <h1>Hello, World from a component !</h1>
+        )
+    }
+}
+
+export default HelloWorld;
