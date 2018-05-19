ee52ab69c30273cb76fad7cffd7fcaf94a1dea5b
karantyagi <tyagi.k@husky.neu.edu>
2018-05-19 18:58:01 -0400

    added course editor container

---
+++ src/containers/CourseEditor.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import ModuleList from './ModuleList'
+import LessonTabs from './LessonTabs'
+
+export default class CourseEditor
+    extends React.Component {
+    render() { return(
+        <div>
+            <div className="row">
+                <div className="col-4">
+                <h3>&nbsp;Modules</h3>
+                    <br/>
+                    <ModuleList/>
+                </div>
+                <div className="col-8">
+                    <h3>&nbsp;Lessons</h3>
+
+                    <LessonTabs/>
+                </div>
+            </div>
+        </div>
+    );
+    }
+}
+
